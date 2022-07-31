33c0bbbd4efd3d5ba246882fe1c12813f60dcbb2
Aerie55 <103826901+Aerie55@users.noreply.github.com>
2022-07-31 20:11:36 +0200

    Update game finder.py

--- game finder.py
+++ game finder.py
@@ -1,1 +1,15 @@
-print('Test dzialania')
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+browser = webdriver.Chrome()
+browser.get("https://moodle.ue.poznan.pl/login/index.php")
+username_input = browser.find_element("id", "username")
+password_input = browser.find_element("id", "password")
+
+username_input.send_keys("numery")
+password_input.send_keys("Jnazwa")
+
+login = browser.find_element("id", "loginbtn")
+login.click()
